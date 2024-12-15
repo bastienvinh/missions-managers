bf5a5c863ffa7f13ebbc04791296886d4662f767
Bastien VINH <bvinh.dev@yahoo.com>
2024-12-15 13:55:01 +0100

    Update sidemenu

--- src/app/menu.tsx
+++ src/app/menu.tsx
@@ -1,8 +1,11 @@
 'use server'
 
 import { getConnectedUser } from "./dal/user-dal"
-import { Sidebar, SidebarFooter, SidebarProvider } from "@/components/ui/sidebar"
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarMenuSub, SidebarMenuSubButton, SidebarMenuSubItem, SidebarProvider } from "@/components/ui/sidebar"
 import SideBarFooterNavigation from "@/components/sidebar-footer-navigation"
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible"
+import { ChevronRight, StickyNote } from "lucide-react"
+import Link from "next/link"
 
 export default async function Menu() {
 
@@ -13,49 +16,81 @@ export default async function Menu() {
   }
 
   return (
-    <SidebarProvider>
+    <SidebarProvider className="h-full">
       <Sidebar>
+        <SidebarHeader>
+
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarMenuButton>Home</SidebarMenuButton>
+
+          {/* Main Menu */}
+          <SidebarGroup>
+            <SidebarGroupLabel>Main Menu</SidebarGroupLabel>
+            <SidebarMenu>
+              <Collapsible asChild className="group/collapsible">
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton tooltip="mission">
+                      <StickyNote />
+                      <span>Missions</span>
+                      <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      {/* Sub menu missions */}
+
+                      {/* Dashboard */}
+                      <SidebarMenuSubItem>
+                        <SidebarMenuSubButton asChild>
+                          <Link href="/missions/form">
+                          <span>Dashboard</span>
+                          </Link>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+
+                      {/* new missions */}
+                      <SidebarMenuSubItem>
+                        <SidebarMenuSubButton asChild>
+                          <Link href="/missions/form">
+                          <span>New Mission</span>
+                          </Link>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+
+                      <SidebarMenuSubItem>
+                        <SidebarMenuSubButton asChild>
+                          <Link href="/missions/list">
+                          <span>Lists</span>
+                          </Link>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </SidebarMenuItem>
+              </Collapsible>
+            </SidebarMenu>
+          </SidebarGroup>
+
+          {/* Operations */}
+          <SidebarGroup>
+            <SidebarGroupLabel>Operations</SidebarGroupLabel>
+            <SidebarMenu>
+              <SidebarMenu>
+                <SidebarMenuButton>
+                  <Link href="/stats">Statistics</Link>
+                </SidebarMenuButton>
+              </SidebarMenu>
+            </SidebarMenu>
+          </SidebarGroup>
+
+        </SidebarContent>
         <SidebarFooter>
           <SideBarFooterNavigation user={user} />
         </SidebarFooter>
       </Sidebar>
     </SidebarProvider>
-    // <div className="w
-    // -full h-full flex flex-col justify-between bg-slate-800">
-    //   <div className="w-full p-5">
-    //     <nav>
-    //       <ul role="list" className="w-full text-white flex flex-col gap-10">
-    //         <li className="w-full">
-    //           <ul className="w-full flex flex-col gap-2">
-    //             <li className="w-full hover:bg-gray-500 rounded p-3 text-xl"><Link href="/" className="flex justify-between"><span className="flex gap-2"><House />Home</span></Link></li>
-    //             <li className="w-full hover:bg-gray-500 rounded p-3 text-xl"><Link href="/missions" className="flex justify-between"><span className="flex gap-2"><Newspaper />Missions</span></Link></li>
-    //             <li className="w-full hover:bg-gray-500 rounded p-3 text-xl"><Link href="/stats" className="flex justify-between"><span className="flex gap-2"><ChartLine />Stats</span></Link></li>
-    //           </ul>
-    //         </li>
-    //         <li className="flex flex-col gap-5">
-    //           <div className="ps-2">Settings</div>
-    //           <ul className="w-full flex flex-col gap-2">
-    //             {hasRequiredRole(user as User, RoleEnum.ADMIN) && <li className="w-full hover:bg-gray-500 rounded p-3 text-xl"><Link href="/users" className="flex justify-between"><span className="flex gap-2"><Users />Users</span></Link></li>}
-    //             <li className="w-full hover:bg-gray-500 rounded p-3 text-xl"><Link href="/parameters" className="flex justify-between"><span className="flex gap-2"><SlidersHorizontal />Parameters</span></Link></li>
-    //           </ul>
-    //         </li>
-    //       </ul>
-    //     </nav>
-    //   </div>
-
-    //   <div className="bg-slate-900 p-4 flex gap-2 justify-between">
-    //     <div className="flex gap-2 items-center">
-    //       <Avatar>
-    //         {/* Im lazy to find an image ... */}
-    //         <AvatarImage src="/persona-01.png" alt="@shadcn" />
-    //         <AvatarFallback>U</AvatarFallback>
-    //       </Avatar>
-    //       {user?.name}
-    //     </div>
-    //     <Tooltip>
-    //       <TooltipContent>Logout</TooltipContent>
-    //     </Tooltip>
-    //   </div>
-    // </div>
   )
 }
